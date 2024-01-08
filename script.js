b84f9774c0d07f653b429af0ee4df0fb15de98b0
Sasikumar-Code <sasiias2024@gmail.com>
2024-01-09 00:37:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("git").onclick = function () {
+  window.open("https://github.com/Sasikumar-Code", "_blank");
+};
+document.getElementById("Portfolio").onclick = function () {
+  window.open("https://sk-portfolio-web.netlify.app/", "_blank");
+};
+document.getElementById("email").onclick = function () {
+  window.open(
+    "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGMvfvkNCJpcrcDRJXhwKskzNVzVVCjstVgcSzGTMgWwmJbnPgPLbSsBLFdcbLhHfRFLmhW",
+    "_blank"
+  );
+};
+document.getElementById("insta").onclick = function () {
+  window.open(
+    "https://www.instagram.com/invites/contact/?i=lfy29zaebef4&utm_content=s4q9tn",
+    "_blank"
+  );
+};
