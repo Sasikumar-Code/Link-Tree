17cad2d939e4379c618b4d04f672538174a74c72
Sasikumar-Code <sasiias2024@gmail.com>
2024-01-10 11:31:19 +0530

    add fb

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ document.getElementById("git").onclick = function () {
   window.open("https://github.com/Sasikumar-Code", "_blank");
 };
 document.getElementById("Portfolio").onclick = function () {
-  window.open("https://sk-portfolio-web.netlify.app/", "_blank");
+  window.open("https://sks-portfolio.netlify.app", "_blank");
 };
 document.getElementById("email").onclick = function () {
   window.open(
     "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGMvfvkNCJpcrcDRJXhwKskzNVzVVCjstVgcSzGTMgWwmJbnPgPLbSsBLFdcbLhHfRFLmhW",
+    "_blank"
+  );
+};
+document.getElementById("facebook").onclick = function () {
+  window.open(
+    "https://www.facebook.com/sasi.chellam.3?mibextid=ZbWKwL",
     "_blank"
   );
 };
